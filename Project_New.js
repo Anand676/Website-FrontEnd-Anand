b1cfaf76b606d85b55e814a9393547c76039b7dd
Anand676 <104975341+Anand676@users.noreply.github.com>
2022-07-17 14:37:00 +0530

    autometic slides added

--- Project_New.js
+++ Project_New.js
@@ -30,22 +30,19 @@ function HideVertical(){
     Vmenu.style.display='none';
     Hmenu.style.display='block';
 }
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
 }
 var Fac=document.getElementById('Fac');
 Fac.addEventListener('click',faculty);
